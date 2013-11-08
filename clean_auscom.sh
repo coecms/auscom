f30b0fe15709ebacb4af44f4fa596a36788e86b7
Marshall Ward <marshall.ward@gmail.com>
2013-11-08 16:36:56 +1100

    Cleanscript update

--- clean_auscom.sh
+++ clean_auscom.sh
@@ -1,10 +1,10 @@
 # Delete executables
-rm -f bin/cice_MPI1.*
-rm -f bin/matm_MPI1.VAYU_nt62
-rm -f bin/mppnccombine.VAYU
-rm -f bin/calendar.exe
-rm -f bin/mom4_MPI1.VAYU 
-rm -f bin/oasis3_MPI1.*
+rm -f bin/cice_MPI?.*
+rm -f bin/matm_MPI?.*
+rm -f bin/mppnccombine.*
+rm -f bin/calendar.RAIJIN
+rm -f bin/mom4_MPI?.* 
+rm -f bin/oasis3_MPI?.*
 
 rm -rf submodels/oasis3/prism/Linux
 rm -f submodels/oasis3/prism/compile/COMP.err
