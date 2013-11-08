ecac3aa251b0ef0ba7964c13a8df2232856306f7
Marshall Ward <marshall.ward@gmail.com>
2013-11-08 16:35:37 +1100

    MATM raijin build

--- build_auscom.sh
+++ build_auscom.sh
@@ -2,18 +2,19 @@
 # TODO: Create a makefile
 
 BASEDIR=$(pwd)
+PLATFORM=RAIJIN
 cd submodels/oasis3/prism/compile
-./comp_oasis325.RAIJIN
+./comp_oasis325.${PLATFORM}
 cd ${BASEDIR}
 
 cd submodels/cice4.1/compile
-./comp_auscom_cice.RAIJIN.nP 6
+./comp_auscom_cice.${PLATFORM}.nP 6
 cd ${BASEDIR}
 
 cd submodels/matm/compile
-./comp_auscom_matm.VAYU
+./comp_auscom_matm.${PLATFORM}
 cd ${BASEDIR}
 
 cd submodels/mom4p1/compile
-./comp_auscom_mom4p1_cfc.VAYU
+./comp_auscom_mom4p1_cfc.${PLATFORM}
 cd ${BASEDIR}
