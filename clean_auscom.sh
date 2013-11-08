dbd49732c7c5c5f2deb5c93b4bf57a36c183f2f2
Marshall Ward <marshall.ward@gmail.com>
2013-11-08 16:27:14 +1100

    Raijin CICE build fix

--- clean_auscom.sh
+++ clean_auscom.sh
@@ -1,10 +1,10 @@
 # Delete executables
-rm -f bin/cice_MPI1.VAYU_6p
+rm -f bin/cice_MPI1.*
 rm -f bin/matm_MPI1.VAYU_nt62
 rm -f bin/mppnccombine.VAYU
 rm -f bin/calendar.exe
 rm -f bin/mom4_MPI1.VAYU 
-rm -f bin/oasis3_MPI1.RAIJIN
+rm -f bin/oasis3_MPI1.*
 
 rm -rf submodels/oasis3/prism/Linux
 rm -f submodels/oasis3/prism/compile/COMP.err
